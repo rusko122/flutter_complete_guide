9dda2849ac15ec726dd875b4bf7ed4a2521c8b93
Braniste Cosmin <cosmin.brap@gmail.com>
2022-12-18 16:35:29 +0200

    questin buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My First App'),
+        ),
+        body: Column(children: const [
+          Text('The question!'),
+          ElevatedButton(
+            onPressed: null,
+            child: Text('Answer 1'),
+          ),
+          ElevatedButton(
+            onPressed: null,
+            child: Text('Answer 2'),
+          ),
+          ElevatedButton(
+            onPressed: null,
+            child: Text('Answer 3'),
+          ),
+        ]),
+      ),
     );
   }
 }

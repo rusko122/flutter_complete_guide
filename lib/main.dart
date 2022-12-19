6f2a4f81c7e820769e342e53f3d5b77753bc2d02
Braniste Cosmin <cosmin.brap@gmail.com>
2022-12-19 14:53:09 +0200

    quiz update in array

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,21 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
   var questionIndex = 0;
 
   void answerQuestion() {
-    questionIndex = questionIndex + 1;
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
     print(questionIndex);
   }
 
